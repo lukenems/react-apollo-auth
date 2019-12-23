0dec8119928de6205f95321a727316d51912c526
Luke Nemy <lukenemy7@gmail.com>
2019-12-22 18:28:42 -0800

    react login form should be g2g, adding in GraphQL next

--- frontend/src/PrivateArea.jsx
+++ frontend/src/PrivateArea.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { navigate } from '@reach/router'
 
 const PrivateArea = () => {
+  if (!Cookies.get('token')) {
+    navigate('/')
+  }
   return (
     <div>
       Private area!
